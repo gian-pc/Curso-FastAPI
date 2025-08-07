31e74dfa8689da6c09920178e5fa0dccdea54f4f
gianpc <gpaucarcortez@gmail.com>
2025-08-06 23:51:13 -0500

    feat: creada la ruta PUT para actualizar un Item

--- main.py
+++ main.py
@@ -28,7 +28,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-# http://127.0.0.1:8000/calculadora?operando_1=3&operando_2=4
+# http://localhost:8000/calculadora?operando_1=3&operando_2=4
 @app.get("/calculadora")
 def calcular(operando_1: float, operando_2: float):
     return {"suma": operando_1 + operando_2}
+
+
+# http://localhost:8000/items/101
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item:Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
